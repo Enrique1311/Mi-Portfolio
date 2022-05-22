7c358190f34babd224f3c7e24dab4c0422349101
Enrique1311 <ejspinelli131176@gmail.com>
2022-05-22 13:18:40 -0300

    Arreglos

--- src/components/utilities/SocialNetworks/SocialNetworks.js
+++ src/components/utilities/SocialNetworks/SocialNetworks.js
@@ -1,45 +1,48 @@
 import {
-    FaFacebookSquare,
-    FaInstagramSquare, 
-    FaGithubSquare, 
-    FaYoutubeSquare, 
-    FaLinkedin,
-    FaPinterestSquare,
-  } from "react-icons/fa";
-  import {BsFacebook, } from "react-icons/bs"
-  import "./SocialNetworks.css";
+  //    FaFacebookSquare,
+  //  FaInstagramSquare,
+  FaGithubSquare,
+  //    FaYoutubeSquare,
+  FaLinkedin,
+  //    FaPinterestSquare,
+} from "react-icons/fa";
+//  import {BsFacebook, } from "react-icons/bs"
+import "./SocialNetworks.css";
 
 const SocialNetworks = () => {
-
   return (
     <div className="social-icons" data-aos="zoom-in">
-      <div>
+      {/* <div>
         <a href="#">
           <i className="facebook"><FaInstagramSquare className="icons"/></i>
         </a>
-      </div>
+      </div> */}
       <div>
         <a href="https://github.com/Enrique1311">
-          <i className="github"><FaGithubSquare className="icons"/></i>
+          <i className="github">
+            <FaGithubSquare className="icons" />
+          </i>
         </a>
       </div>
-      <div>
+      {/* <div>
         <a href="#">
           <i className="instagram"><FaInstagramSquare className="icons"/></i>
         </a>
-      </div>
-      <div>
+      </div> */}
+      {/* <div>
         <a href="#">
           <i className="pinterest"><FaPinterestSquare className="icons"/></i>
         </a>        
-      </div>
+      </div> */}
       <div>
         <a href="https://linkedin.com/in/enrique-spinelli">
-          <i className="linkedin"><FaLinkedin className="icons"/></i>
+          <i className="linkedin">
+            <FaLinkedin className="icons" />
+          </i>
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SocialNetworks
+export default SocialNetworks;
