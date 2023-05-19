26c4c3dbce715ef85d6acf8830797ea45e2ccfc5
Enrique1311 <ejspinelli131176@gmail.com>
2023-05-19 15:05:25 -0300

    The resto-project was added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import './Projects.css';
-import Subtitle from '../utilities/Subtitle/Subtitle';
-import ProjectsCards from './ProjectsCards';
-import { useContext } from 'react';
-import LangContext from '../../context/LangContext';
+import React from "react";
+import "./Projects.css";
+import Subtitle from "../utilities/Subtitle/Subtitle";
+import ProjectsCards from "./ProjectsCards";
+import { useContext } from "react";
+import LangContext from "./../../context/LangContext";
 
 const Proyects = () => {
 	const { texts } = useContext(LangContext);
 
 	return (
 		<div
-			className='projects-container'
-			id='projects'
+			className="projects-container"
+			id="projects"
 		>
 			<Subtitle>{texts.projectsTitle}</Subtitle>
 			<div
-				className='projects'
-				data-aos='zoom-in'
+				className="projects"
+				data-aos="zoom-in"
 			>
 				<ProjectsCards />
 			</div>
--- src/components/Projects/ProjectsCards.js
+++ src/components/Projects/ProjectsCards.js
@@ -1,85 +1,93 @@
-import React from 'react';
-import './ProjectsCards.css';
-import img1 from '../../assets/portf.png';
-import img2 from '../../assets/calculator.png';
-import img3 from '../../assets/movie.png';
-import img4 from '../../assets/mobile-ecommerce.png';
-import img5 from '../../assets/task.png';
-import MyButton from '../utilities/Button/MyButton';
-import { IoIosArrowForward } from 'react-icons/io';
-import LangContext from '../../context/LangContext';
-import { useContext } from 'react';
+import React from "react";
+import "./ProjectsCards.css";
+import img1 from "../../assets/portf.png";
+import img2 from "../../assets/calculator.png";
+import img3 from "../../assets/movie.png";
+import img4 from "../../assets/mobile-ecommerce.png";
+import img5 from "../../assets/task.png";
+import img6 from "../../assets/beer.png";
+import MyButton from "../utilities/Button/MyButton";
+import { IoIosArrowForward } from "react-icons/io";
+import LangContext from "../../context/LangContext";
+import { useContext } from "react";
 
 const ProjectsCards = () => {
 	const { texts } = useContext(LangContext);
 
 	const projectsData = [
 		{
-			id: '0',
-			title: 'Portfolio',
+			id: "0",
+			title: "Portfolio",
 			img: img1,
-			description: 'ReactJS y Css.',
-			url: 'https://enrique-spinelli-port.vercel.app/',
+			description: "ReactJS y Css.",
+			url: "https://enrique-spinelli-port.vercel.app/",
 		},
 		{
-			id: '1',
-			title: 'Calculator app',
+			id: "1",
+			title: "Calculator app",
 			img: img2,
-			description: 'ReactJs, NextJs y Css.',
-			url: 'https://my-calculator-react.netlify.app/',
+			description: "ReactJs, NextJs y Css.",
+			url: "https://my-calculator-react.netlify.app/",
 		},
 		{
-			id: '2',
-			title: 'Movie App',
+			id: "2",
+			title: "Movie App",
 			img: img3,
-			description: 'ReactJs y Css',
-			url: 'https://movie-searcher-react.netlify.app/',
+			description: "ReactJs y Css",
+			url: "https://movie-searcher-react.netlify.app/",
 		},
 		{
-			id: '3',
-			title: 'Ecommerce',
+			id: "3",
+			title: "Ecommerce",
 			img: img4,
-			description: 'React Native y Redux',
-			url: 'https://www.youtube.com/watch?v=I8kt15C6S0M',
+			description: "React Native y Redux",
+			url: "https://www.youtube.com/watch?v=I8kt15C6S0M",
 		},
 		{
-			id: '4',
-			title: 'Task App',
+			id: "4",
+			title: "Task App",
 			img: img5,
-			description: 'React Native',
-			url: 'https://www.youtube.com/watch?v=QXlreHsEIFY',
+			description: "React Native",
+			url: "https://www.youtube.com/watch?v=QXlreHsEIFY",
+		},
+		{
+			id: "5",
+			title: "Resto Website",
+			img: img6,
+			description: "Html, Css y JavaScript",
+			url: "https://resto-project-neon.vercel.app",
 		},
 	];
 
 	return (
-		<div className='cards-container'>
+		<div className="cards-container">
 			{projectsData.map((data) => {
 				return (
 					<div
-						className='projects-cards'
+						className="projects-cards"
 						key={data.id}
 						data={data}
 					>
 						<a
 							href={data.url}
-							className='card-href'
+							className="card-href"
 						>
 							<div
-								className='card'
-								data-aos='zoom-in'
+								className="card"
+								data-aos="zoom-in"
 							>
 								<img
 									src={data.img}
-									alt=''
-									className='card-image'
+									alt=""
+									className="card-image"
 								/>
-								<div className='card-details'>
-									<h2 className='card-title'>{data.title}</h2>
-									<p className='card-description'>{data.description}</p>
+								<div className="card-details">
+									<h2 className="card-title">{data.title}</h2>
+									<p className="card-description">{data.description}</p>
 									<MyButton>
-										<p className='button-text'>
+										<p className="button-text">
 											{texts.projectsButton}
-											<IoIosArrowForward className='icon' />
+											<IoIosArrowForward className="icon" />
 										</p>
 									</MyButton>
 								</div>
