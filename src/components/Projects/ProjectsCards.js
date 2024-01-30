54abf27cc8df0d4cbabe550c12ba8a2f8b85b36f
Enrique1311 <ejspinelli131176@gmail.com>
2024-01-30 01:01:07 -0300

    Changes in projects

--- src/components/Projects/ProjectsCards.js
+++ src/components/Projects/ProjectsCards.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./ProjectsCards.css";
 import img1 from "../../assets/portf.png";
-import img2 from "../../assets/calculator.png";
 import img3 from "../../assets/movie.png";
 import img4 from "../../assets/mobile-ecommerce.png";
 import img5 from "../../assets/task.png";
@@ -27,15 +26,6 @@ const ProjectsCards = () => {
 		},
 		{
 			id: "1",
-			title: "Calculator app",
-			img: img2,
-			description: "ReactJs, NextJs & CSS",
-			gitHubLink: "",
-			buttonIcon: <BiWorld />,
-			url: "https://my-calculator-react.netlify.app",
-		},
-		{
-			id: "2",
 			title: "Movie App",
 			img: img3,
 			description: "API, ReactJs & CSS",
@@ -44,8 +34,8 @@ const ProjectsCards = () => {
 			url: "https://movie-searcher-react.netlify.app",
 		},
 		{
-			id: "3",
-			title: "Ecommerce",
+			id: "2",
+			title: "Mobile Ecommerce",
 			img: img4,
 			description: "React Native & Redux",
 			gitHubLink:
@@ -54,16 +44,16 @@ const ProjectsCards = () => {
 			url: "https://www.youtube.com/watch?v=I8kt15C6S0M",
 		},
 		{
-			id: "4",
-			title: "Task App",
+			id: "3",
+			title: "Mobile Task App",
 			img: img5,
-			description: "React Native",
+			description: "React Native & CSS",
 			gitHubLink: "https://github.com/Enrique1311/task-app-react-nat.git",
 			buttonIcon: <FaYoutube />,
 			url: "https://www.youtube.com/watch?v=QXlreHsEIFY",
 		},
 		{
-			id: "5",
+			id: "4",
 			title: "Bar Website",
 			img: img6,
 			description: "Html, CSS & JavaScript",
@@ -72,7 +62,7 @@ const ProjectsCards = () => {
 			url: "https://resto-project-neon.vercel.app",
 		},
 		{
-			id: "6",
+			id: "5",
 			title: "AI Website",
 			img: img7,
 			description: "ReactJs & CSS",
@@ -81,7 +71,7 @@ const ProjectsCards = () => {
 			url: "https://ai-page-eosin.vercel.app",
 		},
 		{
-			id: "7",
+			id: "6",
 			title: "Restó Website",
 			img: img8,
 			description: "ReactJs & CSS",
@@ -90,7 +80,7 @@ const ProjectsCards = () => {
 			url: "https://ejs-restaurant-react-app.vercel.app",
 		},
 		{
-			id: "8",
+			id: "7",
 			title: "Bank App",
 			img: img9,
 			description: "ReactJs & Tailwind",
